ae8328982fbb7bedfde0841fe0928d6270ac0eda
leonidas400 <dilvano639@gmail.com>
2025-10-30 18:18:23 -0300

    exercício de criar a função delete no crud

--- chapter4/todo_crud.ex
+++ chapter4/todo_crud.ex
@@ -27,10 +27,19 @@ defmodule TodoList do
     case Map.fetch(todo_list.entries, entry_id) do
       :error ->
         todo_list
-        {:ok, old_entry} ->
-          new_entry = updater_fun.(old_entry)
-          new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
-          %TodoList{todo_list | entries: new_entries}
+      {:ok, old_entry} ->
+        new_entry = updater_fun.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
+  def delete_entry(todo_list, entry_id) do
+    case Map.fetch(todo_list, entry_id) do
+      :error ->
+        {:error, "não existe uma tarefa com esse id, verique a lista novamente"}
+      {:ok, _item_encontado} ->
+        {:ok, Map.delete(todo_list, entry_id)}
     end
   end
 
