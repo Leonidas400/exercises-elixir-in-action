38df8fe83dbbd0c82cbc6d4b96c76b5504dd5214
leonidas400 <dilvano639@gmail.com>
2025-11-12 18:24:24 -0300

    criando um projeto mix para o capitulo 7 do livro

---
+++ todo/lib/list.ex
@@ -0,0 +1,46 @@
+defmodule Todo.List do
+  defstruct auto_id: 1, entries: %{}
+
+  def new(), do: %Todo.List{}
+
+  def add_entry(todo_list, entry) do
+    entry= Map.put(entry, :id, todo_list.auto_id)
+    new_entries = Map.put(
+      todo_list.entries,
+      todo_list.auto_id,
+      entry
+    )
+
+    %Todo.List{ todo_list |
+      entries: new_entries,
+      auto_id: todo_list.auto_id + 1
+    }
+  end
+
+  def entries(todo_list, date) do
+    todo_list.entries
+    |> Stream.filter(fn {_, entry} -> entry.date == date end)
+    |> Enum.map(fn {_, entry} -> entry end)
+  end
+
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+      {:ok, old_entry} ->
+        new_entry = updater_fun.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %Todo.List{todo_list | entries: new_entries}
+    end
+  end
+
+  def delete_entry(todo_list, entry_id) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        {:error, "não existe uma tarefa com esse id, verique a lista novamente"}
+      {:ok, _item_encontado} ->
+        {:ok, Map.delete(todo_list.entries, entry_id)}
+    end
+  end
+
+end
